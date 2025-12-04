0abc9593d7e3889008c61f5bc32327baf7c853b3
vilvakannan <vilvaofficial123@gmail.com>
2025-12-04 16:04:32 +0530

    Add API call to get activity data

---
+++ api_testing/get_api_call.py
@@ -0,0 +1,11 @@
+import requests
+
+header = { 'accept' : 'text/plain' }
+
+response = request.get('https://fakerestapi.azurewebsites.net/api/v1/Activities/4',headers= header)
+
+print(response.status_code)
+print(response.json)
+
+assert response.status_code == 200, 'Fail'
+print('pass')
