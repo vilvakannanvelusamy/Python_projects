bd2bce938351c129b3b91bce0c0791fac8cbd114
vilvakannan <kannan.vilva@gmail.com>
2025-12-06 01:11:44 +0530

    updated put_call.py

--- api_testing/put_call.py
+++ api_testing/put_call.py
@@ -1,8 +1,31 @@
 import requests
 import json
 
-url = ''
+url = 'https://fakerestapi.azurewebsites.net/api/v1/Activities/15'
 
-header = {
-    'accept' : 'text/plain'
+header_get = {
+    'accept' : 'text/plain',
+'Content-Type' : 'application/json'
 }
+
+print("before update")
+
+response = requests.get(url,headers = header_get)
+print(response.json())
+
+header_put = {
+    'accept' : 'text/plain',
+'Content-Type' : 'application/json'
+}
+
+putpayload = {
+  "id": 20,
+  "title": "string no: 20",
+  "dueDate": "2025-12-05T19:19:50.228Z",
+  "completed": True
+}
+
+print("after update")
+
+response_put = requests.put(url,headers = header_put, json = putpayload)
+print(response_put.json())
